630cc356538dba58f5b1e8b995d972c7b5fe606b
GabrielAfflitto <gafflitto92@gmail.com>
2018-04-15 20:51:08 -0600

    added state of user to App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
+import Login from './Login';
 import Main from './Main';
 import Navbar from './Navbar';
 import './App.css';
 
 class App extends Component {
-
   constructor() {
     super()
     this.state = {
       artists: [],
-      names: []
+      names: [],
+      user: localStorage.getItem('userId')
     }
   }
 
+  setUser = (user) => {
+    this.setState({user: user})
+    localStorage.setItem('userId', user)
+  }
+
   componentDidMount() {
+    this.getArtists()
+  }
+
+  getArtists = () => {
     fetch('https://cors-anywhere.herokuapp.com/https://goat-reviews-api.herokuapp.com/api/v1/artists')
     .then((response) => response.json())
     .then((data) => this.setState({artists: data.artists}))
@@ -29,13 +39,16 @@ class App extends Component {
     this.setState({ names })
   }
 
+
   render() {
     return (
       <div>
         <Router>
-          <Navbar />
+          <div>
+            <Navbar setUser={this.setUser}/>
+            <Main names={this.state.names} artists={this.state.artists} setUser={this.setUser} user={this.state.user}/>
+          </div>
         </Router>
-        <Main names={this.state.names}/>
       </div>
     )
   }
