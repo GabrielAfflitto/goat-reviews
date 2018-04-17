60cbb68c4ce7858e6def4e13df3dfad4e7d14cea
GabrielAfflitto <gafflitto92@gmail.com>
2018-04-17 17:43:27 -0600

    modified POST request

--- src/PostUser.js
+++ src/PostUser.js
@@ -1,10 +1,10 @@
 export function PostUser(res, tokenId) {
   let userData = res
   return new Promise((resolve, reject) => {
-    fetch('http://localhost:4000/api/v1/auth', {
+    fetch('https://goat-reviews-api.herokuapp.com/api/v1/auth', {
       method: "POST",
       headers: {
-        "HTTP_AUTHORIZATION": `${tokenId}`,
+        "HTTP_AUTHORIZATION": `Bearer ${tokenId}`,
         'Authorization': tokenId,
         'Content-Type': 'application/json',
         'Accept': 'application/json'
